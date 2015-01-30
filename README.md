da53bcc0951008826bbca59b39219d8140256363
Samuel Steele <steelesam72@gmail.com>
2015-01-29 22:48:53 -0500

    Update To file

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hangman
+This repository hosts a small project I'm currently working on, which is a Hangman game written in Python
---
+++ hangman_new.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import random, os, sys
+
+tchar = "> "
+wordlist = open('worldlist.txt', 'r')
+
+def clear():
+    os.system("clear")
+
+def main():
+    print worldlist.read(i)
+
+    die()
+
+def die():
+    sys.exit()
+
+init()
